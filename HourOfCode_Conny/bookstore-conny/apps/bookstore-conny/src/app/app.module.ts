c2ead592032246a6a778fa8c0a538b032da6ca81
rauchcor <corneliarauch@gmx.at>
2019-06-19 08:15:34 +0200

    added root store

--- HourOfCode_Conny/bookstore-conny/apps/bookstore-conny/src/app/app.module.ts
+++ HourOfCode_Conny/bookstore-conny/apps/bookstore-conny/src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, APP_INITIALIZER } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, APP_INITIALIZER } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeModule } from './modules/home/home.module';
-import { HttpClientModule } from '@angular/common/http';
-import { API_BASE_URL } from './core/services/api/SwaggerWebAPI';
-import { CoreModule } from './core/core.module';
-import { ConfigService } from './core/services/config.service';
-import { DxTemplateModule, DxPopupModule, DxButtonModule } from 'devextreme-angular';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeModule } from "./modules/home/home.module";
+import { HttpClientModule } from "@angular/common/http";
+import { API_BASE_URL } from "./core/services/api/SwaggerWebAPI";
+import { CoreModule } from "./core/core.module";
+import { ConfigService } from "./core/services/config.service";
+import {
+  DxTemplateModule,
+  DxPopupModule,
+  DxButtonModule
+} from "devextreme-angular";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import {
+  APP_FEATURE_KEY,
+  initialState as appInitialState,
+  appReducer
+} from "./+state/app.reducer";
+import { AppEffects } from "./+state/app.effects";
+import { AppFacade } from "./+state/app.facade";
+import { NxModule } from "@nrwl/angular";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
+import { storeFreeze } from "ngrx-store-freeze";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -25,7 +40,17 @@ import { DxTemplateModule, DxPopupModule, DxButtonModule } from 'devextreme-angu
     CoreModule,
     DxTemplateModule,
     DxPopupModule,
-    DxButtonModule
+    DxButtonModule,
+    NxModule.forRoot(),
+    StoreModule.forRoot(
+      { app: appReducer },
+      {
+        initialState: { app: appInitialState },
+        metaReducers: !environment.production ? [storeFreeze] : []
+      }
+    ),
+    EffectsModule.forRoot([AppEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [
     {
@@ -33,11 +58,12 @@ import { DxTemplateModule, DxPopupModule, DxButtonModule } from 'devextreme-angu
       useFactory: initializeConfig,
       deps: [ConfigService],
       multi: true
-    }
+    },
+    AppFacade
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
 
 export function initializeConfig(configService: ConfigService) {
   return () => configService.init();
